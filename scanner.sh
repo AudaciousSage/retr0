1363ac40ce42f6e36723ba74cf49dfc4587c80ff
Bunhour <35156325+Bunhour@users.noreply.github.com>
2018-03-04 10:12:35 +0700

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -2,12 +2,15 @@
 
 #interface name
 	interface=$'wlan0'
-
+	interface1='wlan1'
+	
 #putting interface into monitor mode
 	ifconfig "$interface" down
 	iwconfig "$interface" mode monitor
 	ifconfig "$interface" up
+	
 #killing process
 	airmon-ng check kill
+	
 #scanning 
 	airodump-ng "$interface"
