606c2e57a4d49c45a433ea8a46207ece95181059
Bunhour <35156325+Bunhour@users.noreply.github.com>
2018-03-04 10:41:13 +0700

    Update makenormal.sh

--- makenormal.sh
+++ makenormal.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
  
  #interface names
-	interface=$'wlan0'
-	interface1=$'wlan1'
+	interface=$'wlx7c8bca0b520e'
+        interface1=$'wlan1'
+        interface2=$'wlan0'
+        interface3=$'mon0'
+        interface4=$'mon1'
+        interface5=$'wlan0mon'
+        interface6=$'wlan1mon'
+
+
 	
 #making everthing go normal
 	airmon-ng stop "$nterface" 
