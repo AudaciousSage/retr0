99bf93690425179640f415dde05ccd6304ba1f29
Bunhour <35156325+Bunhour@users.noreply.github.com>
2018-03-04 10:55:06 +0700

    Update makenormal.sh

--- makenormal.sh
+++ makenormal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
- 
- #interface names
-	interface=$'wlx7c8bca0b520e'
+
+#interface names
+        interface=$'wlx7c8bca0b520e'
         interface1=$'wlan1'
         interface2=$'wlan0'
         interface3=$'mon0'
@@ -9,10 +9,19 @@
         interface5=$'wlan0mon'
         interface6=$'wlan1mon'
 
-
-	
 #making everthing go normal
-	airmon-ng stop "$nterface" 
-	service NetworkManager restart
-	service avahi-daemon restart
-	service wpa_supplicant restart
+
+#sevices
+        sudo service NetworkManager restart
+        sudo service avahi-daemon restart
+        sudo service wpa_supplicant restart
+
+#interfaces
+        sudo airmon-ng stop "$interface"
+        sudo ifconfig "$interface3" down
+        sudo airmon-ng stop "$interface3"
+        sudo ifconfig "$interface" up
+
+~                                                                                                                                                                                 
+~                                                                                                                                                                                 
+~                                                         
