9cf378d908541746d59c3c9f2f03a86a143fb45f
Bunhour <35156325+Bunhour@users.noreply.github.com>
2018-03-04 10:40:35 +0700

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #interface names
-	interface=$'wlan0'
-	interface1='wlan1'
-	
+	interface=$'wlx7c8bca0b520e'
+        interface1=$'wlan1'
+        interface2=$'wlan0'
+        interface3=$'mon0'
+        interface4=$'mon1'
+        interface5=$'wlan0mon'
+        interface6=$'wlan1mon'
+
+
 #putting interface into monitor mode
 	ifconfig "$interface" down
 	iwconfig "$interface" mode monitor
