e0c1b42cc243d0b22d6fe896e7a843e123780eb5
Bunhour <35156325+Bunhour@users.noreply.github.com>
2018-03-07 20:31:56 +0700

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #interface names
-	interface=$'wlx7c8bca0b520e'
+	interface=$'wlan0'
         interface1=$'wlan1'
-        interface2=$'wlan0'
+        interface2=$'wlx7c8bca0b520e'
         interface3=$'mon0'
         interface4=$'mon1'
         interface5=$'wlan0mon'
